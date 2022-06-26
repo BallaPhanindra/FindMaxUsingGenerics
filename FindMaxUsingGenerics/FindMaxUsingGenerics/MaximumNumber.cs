d02e02d7b7b79f7536df85017dd94f55da5ea514
Balla <ballaphanindra@outlook.com>
2022-06-27 03:38:06 +0530

    Updating TC1.3

--- FindMaxUsingGenerics/FindMaxUsingGenerics/MaximumNumber.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/MaximumNumber.cs
@@ -7,37 +7,35 @@ using System.Threading.Tasks;
 namespace FindMaxUsingGenerics
 {
 
-    internal class MaximumNumber
+    internal class Maximum
     {
 
-        public int ReturnMaxNumber(int num1,int num2,int num3)
+
+        public int ReturnMaxNumber(int num1, int num2, int num3)
 
         {
             int temp;
             if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
-            { 
-                temp = num2;
-                num2 = num1;
+            {
+
+                temp = num3;
+                num3 = num1;
                 num1 = temp;
 
-                return num2;
+                return num1;
             }
 
             else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
             {
+                temp = num3;
+                num3 = num1;
+                num1 = temp;
                 return num2;
             }
-
             else
             {
-                temp = num1;
-                num1 = num3;
-                num3 = temp;
-
-                return num2;
+                return num3;
             }
         }
-
-
     }
 }
--- FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace FindMaxUsingGenerics
 {
@@ -8,13 +7,13 @@ namespace FindMaxUsingGenerics
         static void Main(string[] args)
         {
 
-
             int num1 = 10;
             int num2 = 20;
             int num3 = 30;
-            MaximumNumber max = new MaximumNumber();
+            Maximum max = new Maximum();
             int maxNumber = max.ReturnMaxNumber(num1, num2, num3);
             Console.WriteLine("max number is " + maxNumber);
+
 
 
         }
