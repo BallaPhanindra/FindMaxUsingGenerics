2017161a589d62f6d59b4ab7474c89b3c32de67a
Balla <ballaphanindra@outlook.com>
2022-06-27 03:19:32 +0530

    updating TC 1.1

--- FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -7,15 +7,13 @@ namespace FindMaxUsingGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find Max Number");
+            int num1 = 10;
+            int num2 = 20;
+            int num3 = 30;
+            MaximumNumber max = new MaximumNumber();
+            int maxNumber = max.ReturnMaxNumber(num1, num2, num3);
+            Console.WriteLine("max number is " + maxNumber);
 
-            int[] intArray = { 3, 8, 6, 1, 4, };
-            double[] doubleArray = { 1.2, 1.8, 3.2, 2.8, 7.9 };
-            String[] stringArray = { "12", "24", "56", "34", "89" };
-
-            new GenericMaximum<int>(intArray).PrintMaxValue();
-            new GenericMaximum<double>(doubleArray).PrintMaxValue();
-            new GenericMaximum<string>(stringArray).PrintMaxValue();
 
         }
     }
